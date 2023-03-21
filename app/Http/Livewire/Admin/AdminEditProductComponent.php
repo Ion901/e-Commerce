51695ec3f607241b0d8985c9c2e45b661cbabe47
Ion901 <ionerhan13@gmail.com>
2023-03-21 19:29:08 +0200

    chnges to update,edit,delete articles

--- app/Http/Livewire/Admin/AdminEditProductComponent.php
+++ app/Http/Livewire/Admin/AdminEditProductComponent.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Category;
 use Livewire\Component;
 use Illuminate\Support\Str;
-use App\Models\Category;
 use App\Models\Product;
 use Carbon\Carbon;
 use Livewire\WithFileUploads;
 
-class AdminAddProductComponent extends Component
+class AdminEditProductComponent extends Component
 {
     use WithFileUploads;
+    public $product_id;
     public $name;
     public $slug;
     public $short_description;
@@ -24,13 +25,32 @@ class AdminAddProductComponent extends Component
     public $quantity;
     public $image;
     public $category_id;
+    public $newimage;
+
+    public function mount($product_id){
+        $product = Product::find($product_id);
+        $this -> product_id = $product ->id;
+        $this -> name = $product ->name;
+        $this -> slug = $product ->slug;
+        $this -> short_description = $product ->short_description;
+        $this -> description = $product ->description;
+        $this -> regular_price = $product ->regular_price;
+        $this -> sale_price = $product ->sale_price;
+        $this -> sku = $product ->sku;
+        $this -> stock_status = $product ->stock_status;
+        $this -> featured = $product ->featured;
+        $this -> quantity = $product ->quantity;
+        $this -> image = $product ->image;
+        $this -> category_id = $product ->category_id;
+
+    }
 
     public function generateSlug(){
         $this->slug = Str::slug($this->name);
     }
 
 
-    public function addProduct(){
+    public function updateProduct(){
         $this -> validate([
             'name'=>'required',
             'slug' => 'required',
@@ -40,12 +60,12 @@ class AdminAddProductComponent extends Component
             'sale_price' => 'required',
             'sku' => 'required',
             'stock_status' => 'required',
-            'featured' => 'required|',
+            'featured' => 'required',
             'quantity' => 'required',
             'image' => 'required',
             'category_id' => 'required'
         ]);
-        $product = new Product();
+        $product = Product::find($this->product_id);
         $product->name = $this->name;
         $product->slug = $this->slug;
         $product->short_description = $this->short_description;
@@ -56,16 +76,19 @@ class AdminAddProductComponent extends Component
         $product->stock_status = $this->stock_status;
         $product->featured = $this->featured;
         $product->quantity = $this->quantity;
-        $imageName = Carbon::now()->timestamp.'.'.$this->image->extension();
-        $this->image->storeAs('products',$imageName);
-        $product->image = $imageName;
+        if($this->newimage){
+            unlink('assets/imgs/products/'.$product->image);
+            $imageName = Carbon::now()->timestamp.'.'.$this->newimage->extension();
+            $this->newimage->storeAs('products',$imageName);
+            $product->image = $imageName;
+        }
         $product->category_id = $this->category_id;
         $product->save();
-        session()->flash('message','Product has been added');
+        session()->flash('message','Product has been updated');
     }
     public function render()
     {
-        $categories = Category::orderBy('name','ASC') -> get();
-        return view('livewire.admin.admin-add-product-component',['categories' => $categories]);
+        $categories = Category::orderBy('name','ASC')->get();
+        return view('livewire.admin.admin-edit-product-component',['categories'=>$categories]);
     }
 }
--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,9 +9,18 @@ use Livewire\WithPagination;
 class AdminProductComponent extends Component
 {
     use WithPagination;
+    public $product_id;
+    public $image;
+
+    public function deleteProduct(){
+        $product = Product::find($this->product_id);
+        unlink('assets/imgs/products/'.$product->image);
+        $product->delete();
+        session()->flash('message','Product has been deleted');
+    }
     public function render()
     {
-        $products = Product::orderBy('created_at','DESC')->paginate(10);
+        $products = Product::orderBy('id','ASC')->paginate(10);
         return view('livewire.admin.admin-product-component',['products'=>$products]);
     }
 }
